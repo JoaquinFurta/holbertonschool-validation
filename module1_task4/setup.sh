a279045723a76cfb6698ede31ad11caa2d80e334
JoaquinFurta <joaquinfurta2@gmail.com>
2023-05-19 16:59:30 +0000

    task4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,4 +1,7 @@
 sudo apt-get update 
-apt-get install -y hugo 
+url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
+curl -s $url -L -o hugo.tar.gz
+tar -zxf hugo.tar.gz -C /usr/local/bin
+rm -rf /usr/local/bin/
 apt-get install -y make
 make build
